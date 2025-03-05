ffbe6a67b9d7d181769a1cd14daeac899a2e27e1
Igor Derevnin <shroom2207@yandex.ru>
2025-03-05 22:52:30 +0300

    Добавил модель Places

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Place(models.Model):
+    '''Модель для хранения локаций'''
+    place_name = models.CharField(max_length=255, verbose_name='Название локации')
+    
+    def __str__(self):
+        return f"{self.place_name}"
+
