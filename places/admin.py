f2d2151c267f6224ad9ff38903e42af75fb9b8e5
Igor Derevnin <shroom2207@yandex.ru>
2025-03-09 21:44:44 +0300

    Добавил модель изображений

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 # Register your models here.
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['place_name']
+    
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['image_str', 'img_description', 'image']
+    list_filter = ['location__place_name']
+    search_fields = ['img_description', 'location__place_name']
+    
+    def image_str(self, obj):
+        return str(obj)
+    image_str.short_description = 'Изображение'
--- places/models.py
+++ places/models.py
@@ -6,6 +6,23 @@ class Place(models.Model):
     '''Модель для хранения локаций'''
     place_name = models.CharField(max_length=255, verbose_name='Название локации')
     
+    
     def __str__(self):
-        return f"{self.place_name}"
+        return self.place_name
 
+
+class Image(models.Model):
+    '''Модель для хранения изображений'''
+    img_id = models.PositiveIntegerField(editable=True, verbose_name='Номер изображения', default=1)
+    location = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='Локация', null=True, blank=True)
+    img_description = models.CharField(max_length=255, verbose_name='Описание изображения', blank=True)
+    image = models.ImageField(verbose_name='Файл изображения', null=True, blank=True)       
+    
+    class Meta:
+        unique_together = ('location', 'img_id')
+        ordering = ['location__place_name', 'img_id']
+            
+    def __str__(self):
+        return f'{self.img_id} {self.location.place_name}'
+
+    
